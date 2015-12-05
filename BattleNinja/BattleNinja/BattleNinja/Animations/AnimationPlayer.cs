a950b755b0dac0ab74f0b2449fbd387ee46b5ee2
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-12-05 15:56:04 +0200

    Animation player clas

---
+++ BattleNinja/BattleNinja/BattleNinja/Animations/AnimationPlayer.cs
@@ -0,0 +1,96 @@
+﻿namespace BattleNinja.Animations
+{
+    using System;
+
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public struct AnimationPlayer
+    {
+        private Animation animation;
+        //The amount of time in seconds that the current frame has been shown for.
+        private float time;
+        private int frameIndex;
+
+        //Gets the animation which is currently playing.
+        public Animation Animation 
+        { 
+            get
+            {
+                return this.animation;
+            }
+            set
+            {
+                this.animation = value;
+            }
+        }
+
+        //Gets the index of the current frame in the animation.
+        public int FrameIndex
+        {
+            get
+            {
+                return this.frameIndex;
+            }
+            set
+            {
+                this.frameIndex = value;
+            }
+        }
+
+        //Gets a texture origin at the bottom center of each frame.
+        public Vector2 Origin 
+        { 
+            get
+            {
+                return new Vector2(this.Animation.FrameWidth / 2.0f, this.Animation.FrameWidth);
+            }
+        }
+
+        public void PlayAnimation(Animation animation)
+        {
+            //If this animation is already running, do not restart it.
+            if (this.Animation == animation)
+            {
+                return;
+            }
+
+            //Start the new animation.
+            this.Animation = animation;
+            this.FrameIndex = 0;
+            this.time = 0.0f;
+        }
+
+        //Advances the time position and draws the current frame of the animation. 
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Vector2 position, SpriteEffects spriteEffects)
+        {
+            if (this.Animation == null)
+            {
+                throw new NotSupportedException("No animation is currently playing.");
+            }
+
+            // Process passing time.
+            this.time += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            while (this.time > this.Animation.FrameTime)
+            {
+                this.time -= this.Animation.FrameTime;
+
+                //Advanced the frame index; looping or clamping as appropiate.
+                if (this.Animation.IsLooping)
+                {
+                    this.FrameIndex = (this.FrameIndex + 1) % this.Animation.FrameCount;
+                }
+                else
+                {
+                    this.FrameIndex = Math.Min(this.FrameIndex + 1, this.Animation.FrameCount - 1);
+                }
+            }
+
+            //Calculate the source rectangle of the current frame.
+            Rectangle source = new Rectangle(this.FrameIndex * this.Animation.Texture.Height, 0, this.Animation.Texture.Height, this.Animation.Texture.Height);
+
+            //Draw the current frame.
+            spriteBatch.Draw(this.Animation.Texture, position, source, Color.White, 0.0f, this.Origin, 1.0f, spriteEffects, 0.0f);
+        }
+    }
+}
--- BattleNinja/BattleNinja/BattleNinja/Characters/Enemy.cs
+++ BattleNinja/BattleNinja/BattleNinja/Characters/Enemy.cs
@@ -5,6 +5,28 @@ using System;
 
     public class Enemy
     {
-        
+        private Level level;
+        private Vector2 position;
+        private Rectangle localBounds;
+
+        //private AnimationPlayer player;
+
+        public Level Level
+        {
+            get { return level; }
+        }
+
+        public Vector2 Position
+        {
+            get { return position; }
+        }
+
+        //public Rectangle BoundingRectangle
+        //{
+        //    get
+        //    {
+        //        int left = (int)Math.Round(this.Position.X - sprite);
+        //    }
+        //}
     }
 }
