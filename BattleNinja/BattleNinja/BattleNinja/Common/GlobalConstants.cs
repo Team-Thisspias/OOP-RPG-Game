2a16ae90e03bd093fb1c20e8b5e24826521c6810
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-12-05 14:01:28 +0200

    Add common folder with global constants

---
+++ BattleNinja/BattleNinja/BattleNinja/Common/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleNinja.Common
+{
+    public class GlobalConstants
+    {
+        public const int TileWidth = 40;
+        public const int TileHeight = 32;
+    }
+}
