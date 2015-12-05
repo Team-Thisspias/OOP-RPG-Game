a649a8dd01f392fd525b28090cda98f83c035f23
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-12-05 17:10:04 +0200

    part of enemy class

--- BattleNinja/BattleNinja/BattleNinja/Common/GlobalConstants.cs
+++ BattleNinja/BattleNinja/BattleNinja/Common/GlobalConstants.cs
@@ -6,5 +6,12 @@
         public const int TileHeight = 32;
 
         public const int GemPointValue = 50; //Points given to the player for collecting a icon
+
+        public const float deathTimeMax = 1.0f;
+
+        //How long to wait before turning around;
+        public const float EnemyMaxWaitTime = 0.5f;
+        //The speed at which this enemy moves along the X axis;
+        public const float EnemyMoveSpeed = 64.0f;
     }
 }
