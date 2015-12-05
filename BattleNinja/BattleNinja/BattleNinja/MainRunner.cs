1a904efa498483e4e8b9862ae9133ef608fa46c8
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-12-05 13:34:19 +0200

    Battle ninja game skeleton

---
+++ BattleNinja/BattleNinja/BattleNinja/MainRunner.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BattleNinja
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (BattleNinjaGame game = new BattleNinjaGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
