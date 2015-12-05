94a6dcd3c33c7b693f350d161ea4f7331daf0c87
Ivan <zhostovivan@gmail.com>
2015-12-05 17:08:27 +0200

    Add Level class draw layers

--- BattleNinja/BattleNinja/BattleNinja/Common/GlobalConstants.cs
+++ BattleNinja/BattleNinja/BattleNinja/Common/GlobalConstants.cs
@@ -6,5 +6,10 @@
         public const int TileHeight = 32;
 
         public const int GemPointValue = 50; //Points given to the player for collecting a icon
+
+        public const float ViewMargin = 0.35f;
+
+        public const int EntityLayer = 2; // The layer which entities are drawn on top of.
+
     }
 }
