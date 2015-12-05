e6b82593448bce6746436b400c60944210459214
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-12-05 14:27:10 +0200

    Tile struct

--- BattleNinja/BattleNinja/BattleNinja/Tile.cs
+++ BattleNinja/BattleNinja/BattleNinja/Tile.cs
@@ -3,13 +3,22 @@
     using System;
     
     using Microsoft.Xna.Framework.Graphics;
-    
+    using Microsoft.Xna.Framework;
+
     using BattleNinja.Enums;
+    using BattleNinja.Common;
 
     public struct Tile
     {
         public Texture2D Texture;
         public TileCollision Collision;
 
+        public static readonly Vector2 Size = new Vector2(GlobalConstants.TileWidth, GlobalConstants.TileHeight);
+
+        public Tile(Texture2D texture, TileCollision collision)
+        {
+            this.Texture = texture;
+            this.Collision = collision;
+        }
     }
 }
