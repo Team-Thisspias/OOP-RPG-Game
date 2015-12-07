751306d436fbbc45775c48e3419a0940fdad10ad
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-12-07 14:04:52 +0200

    LoadTiles method added to Level

--- BattleNinja/BattleNinja/BattleNinja/Common/GlobalConstants.cs
+++ BattleNinja/BattleNinja/BattleNinja/Common/GlobalConstants.cs
@@ -7,18 +7,16 @@
 
         public const int GemPointValue = 50; //Points given to the player for collecting a icon
 
-<<<<<<< HEAD
         public const float deathTimeMax = 1.0f;
 
         //How long to wait before turning around;
         public const float EnemyMaxWaitTime = 0.5f;
         //The speed at which this enemy moves along the X axis;
         public const float EnemyMoveSpeed = 64.0f;
-=======
+
         public const float ViewMargin = 0.35f;
 
         public const int EntityLayer = 2; // The layer which entities are drawn on top of.
 
->>>>>>> 94a6dcd3c33c7b693f350d161ea4f7331daf0c87
     }
 }
--- BattleNinja/BattleNinja/BattleNinja/Level.cs
+++ BattleNinja/BattleNinja/BattleNinja/Level.cs
@@ -2,9 +2,12 @@
 {
     using System;
     using System.IO;
+    using System.Collections.Generic;
+    
     using BattleNinja.Characters;
     using BattleNinja.Common;
     using BattleNinja.Items;
+    
     using Microsoft.Xna.Framework;
     using Microsoft.Xna.Framework.Audio;
     using Microsoft.Xna.Framework.Content;
@@ -18,8 +21,24 @@
         private TimeSpan timeRemaining;
         private ContentManager content;
         private SoundEffect exitReachedSound;
+        //Entities in the level.
+        private Player player;
+
+        private List<Gem> gems;
+        private List<Enemy> enemies;
+
+        //Key locations in the level.
+        private Vector2 start;
+        private Point exit = InvalidPosition;
+        private static readonly Point InvalidPosition = new Point(-1, -1);
+
+        //Level game state.
+        private Random random = new Random(354668); // Arbitrary, but constant seed
 
         private float cameraPosition;
+        private int score;
+        private bool reachedExit;
+
 
         public Level(IServiceProvider serviceProvider, Stream fileStream, int levelIndex)
         {
@@ -28,7 +47,7 @@
 
             timeRemaining = TimeSpan.FromMinutes(2.0);
 
-            //TODO LoadTiles()
+            this.LoadTiles(fileStream);
 
             // Load background layer textures. For now, all levels must
             // use the same backgrounds and only use the left-most part of them.
@@ -37,10 +56,37 @@
             layers[1] = new Layer(Content, "Backgrounds/Layer1", 0.5f);   // this allows me to have 3 layers of background instead of the standard 1
             layers[2] = new Layer(Content, "Backgrounds/Layer2", 0.8f);
 
+            this.Gems = new List<Gem>();
+            this.Enemies = new List<Enemy>();
+
             // Load sounds.
             exitReachedSound = Content.Load<SoundEffect>("Sounds/Space laser sounds 2");
 
             // once the exit point has been reached, it will play this sound
+        }
+
+        public int Score 
+        { 
+            get
+            {
+                return this.score;
+            }
+        }
+
+        public bool ReachedExit 
+        { 
+            get
+            {
+                return this.reachedExit;
+            }
+        }
+
+        public Player Player 
+        { 
+            get
+            {
+                return this.player;
+            }
         }
 
         public TimeSpan TimeRemaining
@@ -63,6 +109,80 @@
         public int Height
         {
             get { return tiles.GetLength(1); }
+        }
+
+        public List<Gem> Gems 
+        { 
+            get
+            {
+                return this.gems;
+            }
+            set
+            {
+                this.gems = value;
+            }
+        }
+
+        public List<Enemy> Enemies
+        {
+            get
+            {
+                return this.enemies;
+            }
+            set
+            {
+                this.enemies = value;
+            }
+        }
+
+        private void LoadTiles(Stream fileStream)
+        {
+            int width;
+            List<string> lines = new List<string>();
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string line = reader.ReadLine();
+                width = line.Length;
+                while (line != null)
+                {
+                    lines.Add(line);
+                    if (line.Length != width)
+                    {
+                        throw new Exception(string.Format("The length of line {0} is different from all preceeding lines.", lines.Count));
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            //Allocate the tile grid.
+            this.tiles = new Tile[width, lines.Count];
+
+            //Loop over every tile position
+            for (int y = 0; y < this.Height; ++y)
+            {
+                for (int x = 0; x < this.Width; ++x)
+                {
+                    //to load each tile.
+                    char tileType = lines[y][x];
+                    tiles[x, y] = this.LoadTiles(tileType, x, y);
+                }
+            }
+
+            //Verify that level has a beginning and an end.
+            if (this.Player == null)
+            {
+                throw new NotSupportedException("A level must have a starting point.");
+            }
+            if (this.exit == InvalidPosition)
+            {
+                throw new NotSupportedException("A level must have an exit.");
+            }
+        }
+
+        private Tile LoadTiles(char tileType, int x, int y)
+        {
+            throw new NotImplementedException();
         }
 
         /// Draw everything in the level from background to foreground.
@@ -134,8 +254,6 @@
                 }
             }
         }
-
-
 
     }
 }
